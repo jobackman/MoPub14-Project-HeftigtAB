91a0fd42ab830f92ed81687ffd3093de8f87e726
Johan Bäckman <johba@kth.se>
2014-04-24 12:31:58 +0200

    Mall för navigation-sidan

---
+++ script/navigator.js
@@ -0,0 +1,9 @@
+function rotate() {
+	$("#arrow").css({ 
+		'-webkit-transform': 'rotate(120deg)',
+		'-moz-transform': 'rotate(120deg)',
+		'-o-transform': 'rotate(120deg)',
+		'-ms-transform': 'rotate(120deg)',
+		'transform': 'rotate(120deg)' 
+		});
+}
